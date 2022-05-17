b1c4abf970f96dd06328ed0c9a151061e097f9c6
thang <thangvc91@gmail.com>
2022-05-17 22:14:14 +0700

    day_23

--- day_23/django_rest_project/app/models.py
+++ day_23/django_rest_project/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    phone = models.CharField(max_length=20, unique=True)
+    fullname = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    image = models.ImageField(blank=True, null=True, upload_to='static/images')
+    def __str__(self):
+        return self.fullname
